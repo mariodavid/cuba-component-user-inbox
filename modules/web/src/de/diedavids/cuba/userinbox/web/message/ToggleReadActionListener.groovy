8076953e6bc8b3aaa1e7796ba912660c70546db1
Mario <mario@die-davids.de>
2018-05-09 10:23:43 +0200

    fixed codenarc

--- modules/web/src/de/diedavids/cuba/userinbox/web/message/ToggleReadActionListener.groovy
+++ modules/web/src/de/diedavids/cuba/userinbox/web/message/ToggleReadActionListener.groovy
@@ -2,7 +2,6 @@ package de.diedavids.cuba.userinbox.web.message
 
 import com.haulmont.cuba.core.global.Messages
 import com.haulmont.cuba.gui.components.Action
-import com.haulmont.cuba.gui.components.Frame
 import com.haulmont.cuba.gui.data.Datasource
 import de.diedavids.cuba.userinbox.entity.Message
 
@@ -22,12 +21,12 @@ class ToggleReadActionListener implements Datasource.ItemChangeListener<Message>
 
     private updateToggleReadAction(Message message) {
         if(message.read) {
-            toggleReadAction.caption = formatMessage("markAsUnread")
-            toggleReadAction.icon = "font-icon:SQUARE_O"
+            toggleReadAction.caption = formatMessage('markAsUnread')
+            toggleReadAction.icon = 'font-icon:SQUARE_O'
         }
         else {
-            toggleReadAction.caption = formatMessage("markAsRead")
-            toggleReadAction.icon = "font-icon:CHECK_SQUARE_O"
+            toggleReadAction.caption = formatMessage('markAsRead')
+            toggleReadAction.icon = 'font-icon:CHECK_SQUARE_O'
         }
     }
 
--- modules/web/src/de/diedavids/cuba/userinbox/web/message/UserInbox.groovy
+++ modules/web/src/de/diedavids/cuba/userinbox/web/message/UserInbox.groovy
@@ -11,9 +11,7 @@ import com.haulmont.cuba.gui.components.Frame
 import com.haulmont.cuba.gui.components.Table
 import com.haulmont.cuba.gui.components.actions.CreateAction
 import com.haulmont.cuba.gui.components.actions.EditAction
-import com.haulmont.cuba.gui.components.actions.ItemTrackingAction
 import com.haulmont.cuba.gui.data.CollectionDatasource
-import com.haulmont.cuba.gui.data.Datasource
 import de.diedavids.cuba.userinbox.entity.Message
 
 import javax.inject.Inject
