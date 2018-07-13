bc00a97f33d68a04c179f007fc8b03c6c94d6366
Mario David <mario@die-davids.de>
2018-07-13 23:20:49 +0200

    remove old attributes (#7) (#14)

---
+++ modules/core/db/update/hsql/18/180713-2-updateMessage_DropScript.sql
@@ -0,0 +1,3 @@
+alter table DDCUI_MESSAGE drop column ENTITY_REFERENCE_ID__U52547 cascade ;
+alter table DDCUI_MESSAGE drop column ENTITY_REFERENCE_CLASS__U42171 cascade ;
+alter table DDCUI_MESSAGE drop column ENTITY_CAPTION__U98906 cascade ;
